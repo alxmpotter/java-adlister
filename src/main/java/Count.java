6290cdc1a3b03fa8a0b34fa6eede4f3fc01b386c
alxmpotter <alxmpotter@gmail.com>
2017-10-17 15:36:21 -0500

    servlet exercise
    *finish count page

--- src/main/java/Count.java
+++ src/main/java/Count.java
@@ -1,7 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet(name = "Count", urlPatterns = "/count")
 public class Count extends HttpServlet {
-    protected void doGet(){
 
+    private int counter = 0;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getParameter("reset") != null) {
+            counter = 0;
+        }
+
+        counter++;
+        response.getWriter().print("<h1>The count is " + counter + ".</h1>");
     }
 }
